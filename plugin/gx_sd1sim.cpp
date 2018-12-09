d2e96d04647d83ce07bab6e1e1679b83f489c010
brummer10 <brummer-@web.de>
2018-12-09 19:15:17 +0100

    fix bypass state on automation

--- plugin/gx_sd1sim.cpp
+++ plugin/gx_sd1sim.cpp
@@ -123,7 +123,7 @@ Gx_sd1sim_::Gx_sd1sim_() :
   input(NULL),
   sd1sim(sd1sim::plugin()),
   bypass(0),
-  bypass_(0),
+  bypass_(2),
   needs_ramp_down(false),
   needs_ramp_up(false),
   bypassed(false) {};
